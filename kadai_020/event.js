e073c74285cac7a53a7580799d3bb582b7930f3d
yu-ann5 <yu.anko@icloud.com>
2025-01-15 19:38:56 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,6 +2,5 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-let element = document.getElementById('text');
-element.textContent = 'ボタンをクリックしました';
+text.textContent = 'ボタンをクリックしました';
 });
